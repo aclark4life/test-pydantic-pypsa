ea1ec838ba6c041625743e1baacff4c54eb259f7
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-05-18 10:58:34 -0400

    Add energy system

--- energy_app/models.py
+++ energy_app/models.py
@@ -1,1 +1,8 @@
 # Create your models here.
+from django.db import models
+
+class EnergySystem(models.Model):
+    name = models.CharField(max_length=100)
+    capacity_mw = models.FloatField()
+    efficiency = models.FloatField()
+    cost_per_mw = models.FloatField()
